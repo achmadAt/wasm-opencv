13c0489cee033a4b7ba2d9fb9c3d863c5f5d918d
achmad <achmadalfazari@gmail.com>
2024-01-19 14:40:34 +0700

    feat: add build data

---
+++ script/build.sh
@@ -0,0 +1,32 @@
+git clone --branch 4.9.0 --depth 1 https://github.com/opencv/opencv.git
+
+# Build
+(
+    cd opencv &&
+    git checkout 4.9.0 &&
+
+    # Add non async flag before compiling in the python build_js.py script
+    docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:latest" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
+)
+
+# Copy compilation result
+cp -a ./opencv/build_wasm/ ./build_wasm
+
+# Transpile opencv.js files
+node opencvJsMod.js
+
+# Beautify JS
+(
+    cd ./build_wasm/bin &&
+    npx js-beautify opencv.js -r &&
+    npx js-beautify opencv-deno.js -r
+)
+
+# Copy bins to root
+(
+    cp ./build_wasm/bin/opencv.wasm ../opencv.wasm &&
+    cp ./build_wasm/bin/opencv-bin.js ../opencv-bin.js &&
+    cp ./build_wasm/bin/opencv.js ../opencv.js &&
+    cp ./build_wasm/bin/opencv-deno.js ../opencv-deno.js &&
+    cp -r ./build_wasm/ ../build_wasm_test
+)
